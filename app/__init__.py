af78b3327a3cfcd6a3c4948be4043beb77e431c1
Buck Baskin <buckbaskin@google.com>
2016-09-09 00:59:30 -0400

    Fix var error identified by TravisCI

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 server = Flask(__name__)
 
-@app.route('/')
+@server.route('/')
 def hello():
     return 'Hello World'
 
 if __name__ == '__main__':
-    app.run()
+    server.run()
