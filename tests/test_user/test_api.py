a040937d50e4aa58153e10024c84c6888daca893
Buck Baskin <buckbaskin@google.com>
2016-10-16 13:32:14 -0400

    More user tests

--- tests/test_user/test_api.py
+++ tests/test_user/test_api.py
@@ -9,3 +9,11 @@ class TestUserAPI(unittest.TestCase):
     def testOneUser(self):
         response = self.client.get('/v1/users/one?user_id=123adf89')
         self.assertEqual(response.status_code, 200)
+
+    def testOneUserFailPath(self):
+        response = self.client.get('/v1/users/one')
+        self.assertEqual(response.status_code, 400)
+
+    def testOneUserFailPath2(self):
+        response = self.client.post('/v1/users/one')
+        self.assertEqual(response.status_code, 405)
