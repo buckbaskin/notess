a31087c2bbfb6d198d361d4b259033c62df8fc4d
Zhenglin Huang <zxh108@case.edu>
2016-10-16 18:48:15 -0400

    parallel processing added and used  a new format of request (relevance, text)

--- app/knowledge/api.py
+++ app/knowledge/api.py
@@ -2,6 +2,7 @@ from app import server as router
 from app.knowledge.dbpedia import depedia
 
 import json
+import multiprocessing
 
 from flask import make_response, request
 
@@ -11,15 +12,36 @@ INVALID_REQUEST_NO_KEYWORDS = ('Invalid Request. Keywords not found in request.'
 @router.route('/get_descriptions', methods=['GET'])
 def get_descriptions():
     # requires that the request content type be set to application/json
+    # request should be {'keywords': [{'text': 'w1', 'relevance': '0.946172'}, {'text': 'w2', 'relevance': '0.78827'}]}
     try:
-        keywords = request.args['keywords']
+        keywords_dict = request.args['keywords']
     except KeyError:
         return make_response(*INVALID_REQUEST_NO_KEYWORDS)
-    return get_descriptions_from_keywords(keywords)
+    return add_descriptions_to_keywords_dict(keywords_dict)
 
 
-def get_descriptions_from_keywords(keywords):
-    response_dict = {}
-    for keyword in keywords:
-        response_dict[keyword] = depedia.DBPediaAPI.search(keyword).get_first_description()
-    return json.dumps(response_dict)
+def add_descriptions_to_keywords_dict(keyword_dict_list):
+    with multiprocessing.Pool(10) as p:
+        p.map(add_description, keyword_dict_list)
+    return json.dumps(keyword_dict_list)
+
+
+def add_description(keyword_dict):
+    lookup_result = depedia.DBPediaAPI.search(keyword_dict['text'])
+    if lookup_result.has_results():
+        keyword_dict['description'] = lookup_result.get_first_description()
+    else:
+        keyword_dict['description'] = ""
+
+if __name__ == "__main__":
+    mock_keyword_list = [{'text': 'sequential design process', 'relevance': '0.946172'},
+                         {'text': 'software development processes', 'relevance': '0.78827'},
+                         {'text': 'waterfall model', 'relevance': '0.645009'},
+                         {'text': 'downwards', 'relevance': '0.347695'},
+                         {'text': 'Initiation', 'relevance': '0.282907'}]
+    mock_keyword_request_arg = {'keywords': mock_keyword_list}
+    mock_incoming_request = json.dumps(mock_keyword_request_arg)
+    print(mock_incoming_request)
+    parsed_request = json.loads(mock_incoming_request)
+    parsed_keywords_list = parsed_request['keywords']
+    print(add_descriptions_to_keywords_dict(parsed_keywords_list))
