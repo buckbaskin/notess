2a3e0d98392b3d2850b6dbf69164b3cb17fec4d0
Zhenglin Huang <zxh108@case.edu>
2016-10-16 17:34:22 -0400

    improved get_description

--- app/knowledge/api.py
+++ app/knowledge/api.py
@@ -7,6 +7,7 @@ from flask import make_response, request
 
 INVALID_REQUEST_NO_TEXT = ('Invalid Request. Keywords not found in request.', 400,)
 
+
 @router.route('/get_descriptions', methods=['GET'])
 def get_descriptions():
     # requires that the request content type be set to application/json
@@ -14,7 +15,11 @@ def get_descriptions():
         keywords = request.args['keywords']
     except KeyError:
         return make_response(*INVALID_REQUEST_NO_TEXT)
-    dict = {}
+    return get_descriptions_from_keywords(keywords)
+
+
+def get_descriptions_from_keywords(keywords):
+    response_dict = {}
     for keyword in keywords:
-        dict[keyword] = depedia.DBPediaAPI.search(keyword).get_first_description()
-    return json.dumps(dict)
+        response_dict[keyword] = depedia.DBPediaAPI.search(keyword).get_first_description()
+    return json.dumps(response_dict)
