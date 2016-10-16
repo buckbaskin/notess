e5943826e2c92fac802cca18354dd21f9af24fb5
Zhenglin Huang <zxh108@case.edu>
2016-10-16 16:18:22 -0400

    dbpedia more features completed

--- app/knowledge/dbpedia/depedia.py
+++ app/knowledge/dbpedia/depedia.py
@@ -9,23 +9,44 @@ class DBPediaAPI:
         uri_string = "http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryString=" + keyword
         headers = {'Accept': 'application/json'}
         response = requests.get(url=uri_string, headers=headers)
-        result = DBPediaAPI.QueryResult(json_string=response.text)
+        result = DBPediaAPI.QueryResult(keyword=keyword, json_string=response.text)
         return result
 
     class QueryResult:
         json_dict = ''
+        keyword = ''
 
-        def __init__(self, json_string):
+        def __init__(self, keyword, json_string):
+            self.keyword = keyword
             self.json_dict = json.loads(json_string)
 
-        def has_result(self):
+        def has_results(self):
             return len(self.json_dict['results']) > 0
 
         def __bool__(self):
-            return self.has_result()
+            return self.has_results()
 
         def get_first_description(self):
+            ''' Get the default description associated with the keyword
+            :return: a string of description associated with the keyword included.
+            '''
             return self.json_dict['results'][0]['description']
+
+        def get_responses(self):
+            ''' Get all DBPedia responses associated with the keyword
+            :return: a list of dictionaries associated with each response for the keyword included.
+            '''
+            return self.json_dict['results']
+
+        def get_descriptions(self):
+            ''' Get descriptions associated with the keyword
+            :return: a list of descriptions associated with the keyword included.
+            '''
+            responses = self.json_dict['results']
+            descriptions = []
+            for response in responses:
+                descriptions.append(response['description'])
+            return descriptions
 
         def __str__(self):
             if self:
@@ -33,7 +54,10 @@ class DBPediaAPI:
             else:
                 return "Error: No Result"
 
+        def __repr__(self):
+            return self.__str__()
 
-# if __name__ == "__main__":
-#     d = DBPediaAPI()
-#     print(d.search("cwru"))
+
+if __name__ == "__main__":
+    d = DBPediaAPI()
+    print(d.search("waterfall").get_responses())
