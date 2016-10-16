e8aa1ac863907bffeaf341b154c8fceef2cdf2c5
Buck Baskin <buckbaskin@google.com>
2016-10-16 10:59:39 -0400

    Rename duplicate names

--- app/api.py
+++ app/api.py
@@ -1,6 +1,6 @@
 from app import server
-from app.user import api
-from app.knowledge import api
+from app.user import api as user_api
+from app.knowledge import api as knowledge_api
 
 @server.route('/')
 def hello():
