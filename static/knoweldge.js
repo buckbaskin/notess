6d9b6c87e857de9174b586ffc831e87633c9cc59
Israel Hill <idh@case.edu>
2016-10-16 15:17:24 -0400

    adding skeleton for slider panel

--- static/knoweldge.js
+++ static/knoweldge.js
@@ -1,11 +1,31 @@
 (function ( $ ) {
     $.knowledge = function(text) {
-        $.ajax({
-            type: "POST",
-            url: "~/pythoncode.py",
-            data: { param: text}
-        }).done(function( o ) {
-             // do something
-        });
+        // $.ajax({
+        //     type: "POST",
+        //     url: "~/pythoncode.py",
+        //     data: { param: text}
+        // }).done(function( o ) {
+        //      // do something
+        // });
+
+        var $slider = $('<div id="knowledgeSlider"></div>');
+
+        var showSlider = function() {
+
+        };
+
+        var hideSlider = function() {
+
+        };
+
+        var attachSlider = function() {
+
+        };
+
+
+        return {
+            showSlider: showSlider,
+
+        };
     };
 }( jQuery ));
