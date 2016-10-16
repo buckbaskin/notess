5d8f4d9edb808f93d311f33eb7b58fdf72a7abbb
Zhenglin Huang <zxh108@case.edu>
2016-10-16 13:11:49 -0400

    minor fix

--- app/knowledge/dbpedia/connection.py
+++ app/knowledge/dbpedia/connection.py
@@ -1,10 +1,12 @@
 import requests
-def search(keyword): # => json_response
-    uri_string = "http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryString=" + keyword
-    headers = {'Accept': 'application/json'}
-    response = requests.get(url=uri_string, headers=headers)
-    print(response.json())
-    return response.json()
 
-if __name__ == "__main__":
-    search("waterfall model")
+class DBPediaAPI:
+
+    def __init__(self):
+        self.keywords
+
+    def search(self, keyword): # => json_response
+        uri_string = "http://lookup.dbpedia.org/api/search.asmx/KeywordSearch?QueryString=" + keyword
+        headers = {'Accept': 'application/json'}
+        response = requests.get(url=uri_string, headers=headers)
+        return response.json()
