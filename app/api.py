44944b45769b632cb20aa2ecefb6eff92fdb54f3
Buck Baskin <buckbaskin@google.com>
2016-10-16 12:21:15 -0400

    Add template example

--- app/api.py
+++ app/api.py
@@ -2,6 +2,8 @@ from app import server
 from app.user import api as user_api
 from app.knowledge import api as knowledge_api
 
+from flask import render_template
+
 @server.route('/')
 def hello():
-    return 'Hello World ++'
+    return render_template('index.html')
