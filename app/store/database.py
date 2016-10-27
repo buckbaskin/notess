b6b787dbd5b2d872c7cdee8e9f3560f3dd4661fc
Buck Baskin <buckbaskin@google.com>
2016-10-26 21:07:16 -0400

    Add Users and audio files to db

--- app/store/database.py
+++ app/store/database.py
@@ -1,6 +1,51 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, Connection
 import gridfs
 
+import datetime
+
 class Database(object):
-    pass
+    def __init__(self):
+        self._client = MongoClient('localhost', 27017)
+        self._db = self._client('user')
+
+        self._user_collection = self._db.user
+        self._user_collection.create_index('username', unique=True)
+
+        self._connection = Connection()
+        self._fileclient = gridfs.GridFS(self._connection.gridfs)
+
+    ### User Database ###
+
+    def add_user(self, username):
+        user = {
+            'username': username,
+            'created': datetime.datetime.utcnow()
+            'updated': datetime.datetime.utcnow()
+        }
+        user_id = self._user_collection.insert_one(user).inserted_id
+        return self.get_user(username)
+
+    def update_user(self, username):
+        user = self.get_user(username)
+        user['updated'] = datetime.datetime.utcnow()
+        self._user_collection.update_one({'username': username}, {'$set', user})
+        return self.get_user(username)
+
+    def get_user(self, username):
+        user_result = self._user_collection.find_one({'username': username})
+        return user_result
+
+    def delete_user(self, username):
+        result = self._user_collection.delete_one({'username': username})
+        return result.deleted_count
+
+    ### Audio Database ###
+
+    def write_audio(self, file_, filename, content_type='audio/mpeg'):
+        file_id = self._fileclient.put(file_, filename=filename, content_type=content_type)
+        return file_id
+
+    def read_audio(self, file_id):
+        with fs.get(file_id) as f:
+            return f.read()
 
