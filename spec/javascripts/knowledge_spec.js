7dff12b6af80b001efd9855027ada79b30561b9d
Israel Hill <idh@case.edu>
2016-10-27 22:55:38 -0400

    updating jasmine.yml

--- spec/javascripts/knowledge_spec.js
+++ spec/javascripts/knowledge_spec.js
@@ -1,8 +1,14 @@
 
 describe("Testing javascript knowledge engine.", function() {
 
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
+  var gws_core = GWS_CORE;
+  var k = $.knowledge();
+  k.init();
+
+  it("Checks that words are not duplicates", function() {
+    expect(k.isDuplicate('foo')).toBe(false);
+    expect(k.isDuplicate('bar')).toBe(false);
+    expect(k.isDuplicate('baz')).toBe(false);
   });
-  
+
 });
