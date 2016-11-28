2f78cfc9f7ac84d44912e7d8a9fc57218a4cb684
Zhenglin Huang <zxh108@case.edu>
2016-11-28 18:41:16 -0500

    handled invalid ids in terms of note retrieval

--- app/api.py
+++ app/api.py
@@ -3,6 +3,9 @@ from app.user import api as user_api
 from app.knowledge import api as knowledge_api
 
 from flask import render_template, make_response, request
+from app.store.database import Database
+
+db = Database()
 
 @server.route('/')
 def hello():
@@ -16,7 +19,17 @@ def load_editor():
         note_id = request.args['note_id']
     except KeyError:
         return make_response("No note id")
-    return render_template('index.html', isNew=False, note_id=note_id)
+    try:
+        user_name = request.args['user_name']
+    except KeyError:
+        return make_response("No user id")
+    try:
+        found_result = db.get_note(note_id=note_id, username=user_name)
+        if found_result is not None:
+            return render_template('index.html', isNew=False, note_id=note_id)
+    except:
+        print("caught some unknown error")
+    return render_template('index.html', isNew=True, note_id=-1)
 
 
 @server.route('/notes')
