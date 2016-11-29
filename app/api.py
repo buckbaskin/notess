974723d7304ad8208781893e6c3bc2fe2323ef33
Zhenglin Huang <zxh108@case.edu>
2016-11-28 20:12:43 -0500

    Two way note saving working

--- app/api.py
+++ app/api.py
@@ -25,8 +25,9 @@ def load_editor():
         return make_response("No user id")
     try:
         found_result = db.get_note(note_id=note_id, username=user_name)
+        return_text = found_result['text'].replace('\n\n', '-break-')
         if found_result is not None:
-            return render_template('index.html', isNew=False, note_id=note_id, note_title=found_result['note_name'], note_text="This note is fresh")
+            return render_template('index.html', isNew=False, note_id=note_id, note_title=found_result['note_name'], note_text=return_text)
     except:
         print("caught some unknown error")
     return render_template('index.html', isNew=True, note_id=-1)
