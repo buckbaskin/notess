79528f4bfd81976b7174d838f6493b1ca0cf7258
Zhenglin Huang <zxh108@case.edu>
2016-11-28 20:45:33 -0500

    added test coverage

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,3 +12,11 @@ class APITestCase(unittest.TestCase):
     def test_docs(self):
         response = self.client.get(self.editor_url, query_string='user_name=testuser&note_id=583cd3e64855a87f80000000')
         self.assertEqual(response.status_code, 200)
+
+    def test_docs_no_user_id(self):
+        response = self.client.get(self.editor_url, query_string='user_name=testuser')
+        self.assertEqual(response.status_code, 200)
+
+    def test_docs_no_note_id(self):
+        response = self.client.get(self.editor_url, query_string='noteid=test')
+        self.assertEqual(response.status_code, 200)
