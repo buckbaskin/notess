9e6769a9cbebae3ab9fa964ff97e5662981cb416
Zhenglin Huang <zxh108@case.edu>
2016-11-28 20:20:42 -0500

    added test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,14 @@
 import unittest
-
+from app import server
 
 class APITestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.client = server.test_client()
+        self.editor_url = '/docs'
 
     def tearDown(self):
         pass
+
+    def test_docs(self):
+        response = self.client.get(self.editor_url, query_string='user_name=testuser&note_id=583cd3e64855a87f80000000')
+        self.assertEqual(response.status_code, 200)
